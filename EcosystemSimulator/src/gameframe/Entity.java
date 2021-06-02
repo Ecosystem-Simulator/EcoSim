cd066e4bf9cac2a02f03abbef2aab7406541840f
tphuongngo <83686400+tphuongngo@users.noreply.github.com>
2021-06-02 11:26:26 -0700

    Add files via upload

---
+++ EcosystemSimulator/src/gameframe/Entity.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class Entity {
+    protected static ArrayList<Entity> entities;
+    private int x, y;
+    private double direction;
+    private boolean active = true;
+    public Entity(int x, int y, ArrayList<Entity> entities){
+        this.x = x;
+        this.y = y;
+        this.entities = entities;
+    }
+    
+    public int getX(){
+        return(x);
+    }
+    
+    public int getY(){
+        return(y);
+    }
+    
+    public boolean isActive() {
+        return (active);
+    }
+    public ArrayList<Entity> getEntities() {
+        return entities;
+    }
+
+}
