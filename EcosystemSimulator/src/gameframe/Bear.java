cfd0b306802141d5f1a0369ebeb4ded56d7500b9
816491 <816491@04-315-J0CP4LG.SD41.ADS>
2021-06-04 11:18:35 -0700

    bear class1

--- EcosystemSimulator/src/gameframe/Bear.java
+++ EcosystemSimulator/src/gameframe/Bear.java
@@ -1,10 +1,18 @@
 
 package gameframe;
+import java.awt.Graphics;
+import java.awt.Color;
 import java.util.ArrayList;
 
 public class Bear extends Animal{
-    public Bear(int x, int y, ArrayList<Entity> entities, Entity[][] entitygrid, int gridLength){
-        super(x, y, entities, entitygrid, gridLength);
+    public Bear(int x, int y, ArrayList<Entity> entities, String gender, Entity[][] entitygrid, int gridLength){
+        super(x, y, entities, gender, entitygrid, gridLength);
     }
+    
+    public void draw(Graphics g){
+        g.setColor(new Color(255, 0, 0));
+        g.fillOval(getX() - length/2, getY() - height/2, length, height);
+    }
+    
     
 }
