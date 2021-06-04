c766aafb79866368827515d2fdc87937e9140f7b
816491 <816491@04-315-J0CP4LG.SD41.ADS>
2021-06-04 11:09:11 -0700

    bear class

---
+++ EcosystemSimulator/src/gameframe/Bear.java
@@ -0,0 +1,10 @@
+
+package gameframe;
+import java.util.ArrayList;
+
+public class Bear extends Animal{
+    public Bear(int x, int y, ArrayList<Entity> entities, Entity[][] entitygrid, int gridLength){
+        super(x, y, entities, entitygrid, gridLength);
+    }
+    
+}
