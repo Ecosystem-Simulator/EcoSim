344cc8b81e06e09a0189ac71e34c0f02b133b3c5
SwiftThorn1 <jefflu2004@hotmail.com>
2021-06-07 09:26:26 -0700

    made deer class

--- EcosystemSimulator/src/gameframe/Deer.java
+++ EcosystemSimulator/src/gameframe/Deer.java
@@ -1,6 +1,9 @@
 package gameframe;
+
+import java.util.ArrayList;
+
 public class Deer extends Animal {
-    public Deer(){
-        
+    public Deer(int gridX, int gridY, ArrayList<Entity> entities, String gender, Entity[][] entitygrid, int gridLength){
+        super(gridX, gridY, entities, gender, entitygrid, gridLength);
     }
 }
