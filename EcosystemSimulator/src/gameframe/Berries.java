9246d4a261c5479de33295d832f624dd1550d587
1090720 <1090720@04-315-J0CP4LC.SD41.ADS>
2021-06-07 09:47:01 -0700

    berries draw method

--- EcosystemSimulator/src/gameframe/Berries.java
+++ EcosystemSimulator/src/gameframe/Berries.java
@@ -5,6 +5,8 @@
  */
 package gameframe;
 
+import java.awt.Color;
+import java.awt.Graphics;
 import java.util.ArrayList;
 
 /**
@@ -15,5 +17,11 @@ public class Berries extends Food{
     public Berries(int x, int y, ArrayList<Entity> entities, Entity[][] entitygrid, int gridLength){
         super(x, y, entities, entitygrid, gridLength);
         setNutritionVal(20);
+    }
+    public void draw(Graphics g){
+        g.setColor(new Color(0, 153, 0));
+        g.fillOval(getX() - getLength()/2, getY() - getHeight()/2, getLength(), getHeight());
+        g.setColor (new Color(204, 0, 0));
+        g.fillOval(getX() - getLength()/2, getY() - getHeight()/2, getLength()/2, getHeight()/2);
     }
 }
